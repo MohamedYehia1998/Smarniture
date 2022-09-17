3c0c0b9858b9b975187bb19175ad676ab30849fb
ge37zat <ge37zat@mytum.de>
2022-09-17 23:59:53 +0200

    change home page a bit

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,21 +2,22 @@ import Carousel from 'react-bootstrap/Carousel';
 import img1 from '../../images/a.jpg'
 import img2 from '../../images/b.jpg'
 import img3 from '../../images/c.jpg'
+import classes from './Home.module.css'
 
 function Home() {
     return (
-        <div style={{margin:'100px auto'}}>
-            <Carousel>
+
+            <Carousel interval={550} variant="dark" className={classes.carousel}>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={img1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -25,10 +26,10 @@ function Home() {
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -37,15 +38,14 @@ function Home() {
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>
                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                         </p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
             </Carousel>
-        </div>
 
     );
 }
