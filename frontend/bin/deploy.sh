4d4298c1c40e7993f45c860a94b533a04be71007
ge37zat <ge37zat@mytum.de>
2022-09-17 18:41:13 +0200

    cd

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp ./build s3://randombucket1998/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://randombucket1998/
+aws s3 cp --recursive --acl public-read ./build s3://randombucket1998/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build/index.html s3://randombucket1998/
